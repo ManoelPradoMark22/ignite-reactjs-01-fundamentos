3c9fcdeeceb9afb152091728a2119ced905c1cc9
Manoel Prado <manoel.prado.neto@accenture.com>
2023-12-28 09:56:45 -0300

    css-modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css'
 
 export function App() {
 
   return (
     <div>
+      <Header/>
       <Post 
         author="Manoel Prado"
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo pariatur repellat delectus quam tempora adipisci, expedita hic! Ex maiores beatae provident, eaque molestiae cumque nobis unde fuga aliquid tempore eos!"
