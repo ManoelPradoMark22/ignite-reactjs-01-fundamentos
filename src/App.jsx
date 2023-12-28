7007bbc0579bf6948195ddaf5cdd49c3846dc22c
Manoel Prado <manoel.prado.neto@accenture.com>
2023-12-28 09:05:30 -0300

    properties

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ export function App() {
 
   return (
     <div>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post 
+        author="Manoel Prado"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo pariatur repellat delectus quam tempora adipisci, expedita hic! Ex maiores beatae provident, eaque molestiae cumque nobis unde fuga aliquid tempore eos!"
+      />
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,29 +1,12 @@
-export function Post() {
-  return <p>Post</p>
+import PropTypes from 'prop-types';
+
+export function Post(props) {
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  )
 }
 
-//Default Expots vs Named Exports
 
-/*Default Exports
-Pode mudar o nome para o componente na IMPORTAÇÃO!
-
-function Post() {
-  return <p>Post</p>
-}
-
-export default Post
-
-// no arquivo , onde vai importar:
-import Post from './Post';
-*/
-
-/*Default Exports
-NÃO pode mudar o nome para o componente na IMPORTAÇÃO!
-
-export function Post() {
-  return <p>Post</p>
-}
-
-// no arquivo , onde vai importar:
-import { Post } from './Post';
-*/
