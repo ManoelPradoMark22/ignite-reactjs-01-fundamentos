a737466347b627efee4015f79a06e9afd4e91372
Manoel Prado <manoel.prado.neto@accenture.com>
2023-12-29 13:38:23 -0300

    style Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,4 +1,6 @@
 import styles from './Comment.module.css'
+import { Trash } from '@phosphor-icons/react';
+import { HandsClapping } from '@phosphor-icons/react';
 
 import { Avatar } from './Avatar'
 
@@ -9,6 +11,30 @@ export function Comment() {
         src="https://github.com/diego3g.png"
         hasBorder={false}
       />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+                <strong>Diego Fernandes</strong>
+                <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <HandsClapping/>
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
     </div>
   )
 }
