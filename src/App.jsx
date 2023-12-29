3f1ebe5e7c90cca95f7bc5fdb2591336c3a98bad
Manoel Prado <manoel.prado.neto@accenture.com>
2023-12-28 21:30:28 -0300

    sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
-import './global.css'
+import styles from './App.module.css';
+
+import './global.css';
 
 export function App() {
 
   return (
     <div>
       <Header/>
-      <Post 
-        author="Manoel Prado"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo pariatur repellat delectus quam tempora adipisci, expedita hic! Ex maiores beatae provident, eaque molestiae cumque nobis unde fuga aliquid tempore eos!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author="Manoel Prado"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo pariatur repellat delectus quam tempora adipisci, expedita hic! Ex maiores beatae provident, eaque molestiae cumque nobis unde fuga aliquid tempore eos!"
+          />
+          <Post 
+            author="Anna Paula"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo pariatur repellat delectus quam tempora adipisci, expedita hic! Ex maiores beatae provident, eaque molestiae cumque nobis unde fuga aliquid tempore eos!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
